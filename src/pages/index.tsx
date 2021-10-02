b28debd7f966822c9a1fa499190668ad25090b6f
Gaspar <gaspardolcemascolo@gmail.com>
2021-10-02 16:05:45 -0300

    adding social links

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,14 +3,14 @@ import { FaGithub, FaLinkedin, FaTwitterSquare } from "react-icons/fa";
 
 const Home = () => {
   return (
-    <section className="bg-gray-50 h-screen flex items-center flex-col justify-center">
-      <div className="w-max">
+    <section className="p-3 bg-gray-50 h-screen flex items-center flex-col justify-center">
+      <div className="">
         <h1 className="text-5xl font-bold">Hello, I'm Gaspar Dolcemascolo</h1>
         <h2 className="text-4xl mt-4">
           FrontEnd Developer at{" "}
           <a
-            style={{ background: "#6a35ff" }}
-            className="text-gray-50"
+            style={{ color: "#6a35ff" }}
+            className="underline"
             target="_blank"
             href="https://www.cleveritgroup.com/"
           >
@@ -18,9 +18,18 @@ const Home = () => {
           </a>
         </h2>
         <div className="flex mt-8">
-          <FaGithub className="mr-4" size="2rem" color="#6a35ff" />
-          <FaLinkedin className="mr-4" size="2rem" color="#6a35ff" />
-          <FaTwitterSquare className="mr-4" size="2rem" color="#6a35ff" />
+          <a target="_blank" href="https://github.com/gasparnd">
+            <FaGithub className="mr-4" size="2rem" color="#6a35ff" />
+          </a>
+          <a
+            target="_blank"
+            href="https://www.linkedin.com/in/gaspardolcemascolo/"
+          >
+            <FaLinkedin className="mr-4" size="2rem" color="#6a35ff" />
+          </a>
+          <a target="_blank" href="https://twitter.com/gasparnd">
+            <FaTwitterSquare className="mr-4" size="2rem" color="#6a35ff" />
+          </a>
         </div>
       </div>
     </section>
