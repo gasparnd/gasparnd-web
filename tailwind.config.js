14927293cc6415a973c8dc9ec5ed538119de2795
gasparnd <gaspardolcemascolo@gmail.com>
2022-04-10 12:22:22 -0300

    upload tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,11 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: "class",
   theme: {
-    extend: {
-      colors: {
-        primary: "#6a35ff",
-      },
-    },
-  },
-  variants: {
-    extend: {},
+    extend: { primary: "#6a35ff" },
   },
   plugins: [],
 };
